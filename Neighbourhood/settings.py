0208daf728a9ea157ede7c170e19314bf6683784
Moringa School <dorcasmosonik@gmail.com>
2020-12-02 12:34:59 +0300

    deployment

--- Neighbourhood/settings.py
+++ Neighbourhood/settings.py
@@ -119,14 +119,14 @@ REST_FRAMEWORK = {
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'hood',
-        'USER':'moringa',#change your username
-        'PASSWORD':''# change your password
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'hood',
+#         'USER': 'moringa',#change your username
+#         'PASSWORD': '',# change your password,
+#     }
+# }
 
 
 
