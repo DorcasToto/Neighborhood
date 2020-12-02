c5a752a9a3d41eb93c53aa12a9f68d098cfd556b
Moringa School <dorcasmosonik@gmail.com>
2020-12-02 12:50:38 +0300

    update apps settings

--- Neighbourhood/settings.py
+++ Neighbourhood/settings.py
@@ -71,7 +71,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'hood',
-    'rest_framework'
+    'rest_framework',
     'corsheaders',
 ]
 
@@ -154,7 +154,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'Nairobi'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
