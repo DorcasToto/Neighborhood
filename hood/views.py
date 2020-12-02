fe356c9937880cfd84d3f239cc06ad23a408c63e
Moringa School <dorcasmosonik@gmail.com>
2020-12-02 18:50:31 +0300

    remove bugs

--- hood/views.py
+++ hood/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from django.contrib.auth.models import User
-from .serializer import UserSerializer, UserRegistrationSerializer,HoodSerializer,PostSerializer,ProfileSerializer, BusinessSerializer
+from .serializer import UserSerializer, UserRegistrationSerializer,HoodSerializer,PostSerializer,ProfileSerializer
 from rest_framework import viewsets, permissions
 from rest_framework.decorators import action
 from rest_framework.permissions import AllowAny
