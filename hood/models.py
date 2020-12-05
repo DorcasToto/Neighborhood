4c4c93655d40164055f0ab9332136395d518e565
Moringa School <dorcasmosonik@gmail.com>
2020-12-05 19:57:53 +0300

    post migrations

--- hood/models.py
+++ hood/models.py
@@ -100,7 +100,7 @@ class Post(models.Model):
     title = models.CharField(max_length=100)
     text = models.TextField()
     photo = CloudinaryField('postPhoto',default='')
-    user = models.ForeignKey(User,on_delete=models.CASCADE,default = '')
+    user = models.ForeignKey(User,on_delete=models.CASCADE,default = '',null=True,blank=True)
     date = models.DateField(auto_now_add=True)
     neighbourhood = models.ForeignKey(Neighbourhood,on_delete=models.CASCADE, default='', null=True, blank=True)
 
