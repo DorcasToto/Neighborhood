9c49bb6c4d1b84d997b63c6d0fcf1510af867bbb
Moringa School <dorcasmosonik@gmail.com>
2020-12-06 11:19:46 +0300

    remove permission

--- hood/views.py
+++ hood/views.py
@@ -74,7 +74,7 @@ class HoodViewset(mixins.CreateModelMixin,
 
     queryset = Neighbourhood.objects.all()
     serializer_class = HoodSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.AllowAny]
 
     @action(detail=False, methods=['GET'])
     @permission_decorator([permissions.AllowAny])
